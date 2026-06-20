a300a6c8b5fb034a0854a0ea9663d211864c7ce1
Clip-Win Dev <dev@clip-win.local>
2026-06-20 06:11:16 +0000

    Enable GPU acceleration in dev container

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
+
+sudo mkdir -p /tmp/runtime-vscode
+sudo chown vscode:vscode /tmp/runtime-vscode
+sudo chmod 700 /tmp/runtime-vscode
 
 if ! cargo watch --version >/dev/null 2>&1; then
   cargo install cargo-watch --locked
